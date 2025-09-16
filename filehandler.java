c0cd9eafef8a4bd8a1d8919f1ba8226e08edd068
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-16 20:53:41 +0530

    Update filehandler.java

--- filehandler.java
+++ filehandler.java
@@ -1,1 +1,12 @@
 //this file is for handling the file
+import java.io.File;
+
+public class FileHandler {
+    protected String filePath;
+
+    public FileHandler() {}
+
+    public FileHandler(String filePath) {
+        this.filePath = filePath;
+    }
+
