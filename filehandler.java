b132902d6efdc0181e9e3785c3de9921f33fe6c0
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-16 20:54:43 +0530

    Update filehandler.java

--- filehandler.java
+++ filehandler.java
@@ -9,4 +9,21 @@ public class FileHandler {
     public FileHandler(String filePath) {
         this.filePath = filePath;
     }
+    // Common method to scan files 
+    public void scanFiles(String directoryPath) {
+        File dir = new File(directoryPath);
+
+        if (!dir.exists() || !dir.isDirectory()) {
+            System.out.println("Invalid directory: " + directoryPath);
+            return;
+        }
+
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                System.out.println("Found: " + file.getName());
+            }
+        }
+    }
+}
 
