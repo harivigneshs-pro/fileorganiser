77cad2fdc4497a1380d16c42350c61f7f5a9a501
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-17 23:49:17 +0530

    Update filehasher.java

--- filehasher.java
+++ filehasher.java
@@ -1,2 +1,31 @@
 //this file is for hashing 
+import java.io.FileInputStream;
+import java.security.MessageDigest;
 
+public class FileHasher {
+    public static String getFileHash(String filePath) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            FileInputStream fis = new FileInputStream(filePath);
+
+            byte[] byteArray = new byte[1024];
+            int bytesCount;
+
+            while ((bytesCount = fis.read(byteArray)) != -1) {
+                digest.update(byteArray, 0, bytesCount);
+            }
+            fis.close();
+
+            byte[] bytes = digest.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
