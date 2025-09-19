b8fd0ae3c45c03ec45f74edd7e2fec9b54894e1d
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 17:24:59 +0530

    Update reportgenerator.java

--- reportgenerator.java
+++ reportgenerator.java
@@ -1,1 +1,17 @@
 //this file is used to denerate report of how much file is organised and sorted,etc
+import java.io.FileWriter;
+import java.io.IOException;
+public class ReportGenerator {
+    public void writeReport() {
+        try {
+            FileWriter fw = new FileWriter("report.txt");
+            fw.write("File Organizer Report\n");
+            fw.write("Files organized into categories.\n");
+            fw.write("Duplicate check completed.\n");
+            fw.close();
+            System.out.println("\nReport created: report.txt");
+        } catch (IOException e) {
+            System.out.println("Error writing report.");
+        }
+    }
+}
