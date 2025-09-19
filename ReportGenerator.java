a945388aa936c34907a76f691368fbefa97c79bc
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 19:03:16 +0530

    modified

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -3,9 +3,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class ReportGenerator{
     public void writeReport(){
-        FileWriter fw=new FileWriter("report.txt");
-         fw.write("File Organizer Report\n");
-        fw.write("Files organized into categories.\n");
-        fw.write("Duplicate check completed.\n");
-        fw.close();
-        System.out.println("\nReport created: report.txt");}}
+         try {
+            FileWriter fw = new FileWriter("report.txt");
+            fw.write("File Organizer Report\n");
+            fw.write("Files organized into categories.\n");
+            fw.write("Duplicate check completed.\n");
+            fw.close();
+            System.out.println("\nReport created: report.txt");
+        }
+        catch (IOException e) {
+            System.out.println("Error writing report.");
+        }
+    }
+}
