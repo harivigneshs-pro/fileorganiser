08b628a1c3e3d5c343c4610e0f67084c908bc856
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 18:13:06 +0530

    Update duplicatefinder.java

--- duplicatefinder.java
+++ duplicatefinder.java
@@ -2,22 +2,21 @@
 //the file initially finds the duplicates  tecniques
 import java.io.File;
 import java.util.HashSet;
-
 public class DuplicateFinder {
     String folderPath;
     public DuplicateFinder(String path) {
-        this.folderPath = path;
+        this.folderPath=path;
     }
     public void findDuplicates() {
-        File folder = new File(folderPath);
-        File[] files = folder.listFiles();//listFiles a built in method for listing all the files in a folder
-if (files == null) 
+        File folder=new File(folderPath);
+        File[] files=folder.listFiles();//listFiles a built in method for listing all the files in a folder
+if (files==null) 
     return;
-        HashSet<String> seen = new HashSet<>();
+        HashSet<String> seen=new HashSet<>();
  System.out.println("\nChecking duplicates...");
         for (File f : files) {
             if (f.isFile()) {
-                String name = f.getName();//fetches name of the file 
+                String name=f.getName();//fetches name of the file 
                 if (seen.contains(name)) {
                     //contains-built in method to check value present or not
                     System.out.println("Duplicate found: " + name);
