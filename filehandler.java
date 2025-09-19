849b290d6e82a40c46ded9327ef0fbe2bb6a430b
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 17:31:50 +0530

    Update filehandler.java

--- filehandler.java
+++ filehandler.java
@@ -1,30 +1,3 @@
 //this file is for handling the file
 //this is a java code for handlling ythe files wit its path name and location
-import java.io.File;
-
-public class FileHandler {
-    protected String filePath;
-
-    public FileHandler() {}
-
-    public FileHandler(String filePath) {
-        this.filePath = filePath;
-    }
-    // Common method to scan files 
-    public void scanFiles(String directoryPath) {
-        File dir = new File(directoryPath);
-
-        if (!dir.exists() || !dir.isDirectory()) {
-            System.out.println("Invalid directory: " + directoryPath);
-            return;//returning the function
-        }
-
-        File[] files = dir.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                System.out.println("Found: " + file.getName());
-            }
-        }
-    }
-}
-
+imprt java.util.*;
