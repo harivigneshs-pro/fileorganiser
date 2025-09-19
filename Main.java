b37b2051699269f15d99a922eccdd6f5a42f27b6
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 18:21:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,10 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:\\Users\\Hari\\Downloads";
-
-        FileOrganizer fo = new FileOrganizer(folderPath);
+        String folderPath="C:\\Users\\Hari\\Downloads";
+        FileOrganizer fo=new FileOrganizer(folderPath);
         fo.organizeFiles();
-
-        DuplicateFinder df = new DuplicateFinder(folderPath);
+        DuplicateFinder df=new DuplicateFinder(folderPath);
         df.findDuplicates();
-
-        ReportGenerator rg = new ReportGenerator();
-        rg.writeReport();
-    }
-}
+        ReportGenerator rg=new ReportGenerator();
+        rg.writeReport();}}
