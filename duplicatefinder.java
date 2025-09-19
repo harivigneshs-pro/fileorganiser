d0cdd8571975c1d329149ab3a169377f38aa9737
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 17:23:30 +0530

    Update duplicatefinder.java

--- duplicatefinder.java
+++ duplicatefinder.java
@@ -1,42 +1,30 @@
 //this file is to remove duplicate files using hashing
 //the file initially finds the duplicates  tecniques
 import java.io.File;
-public class FileOrganizer {
+import java.util.HashSet;
+
+public class DuplicateFinder {
     String folderPath;
-  public FileOrganizer(String path) {
+    public DuplicateFinder(String path) {
         this.folderPath = path;
     }
-  public void organizeFiles() {
+    public void findDuplicates() {
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
- if (files == null) {
-            System.out.println("Folder not found!");
-            return;
-      }
-for (File f : files) {
-      if (f.isFile()) {
-          String name = f.getName();
-        String ext = "";
-        int dot = name.lastIndexOf(".");
-      if (dot != -1) {
-                    ext = name.substring(dot + 1).toLowerCase();
-                }
-String category = "Others";
-    if (ext.equals("jpg") || ext.equals("png")) category = "Images";
-   else if (ext.equals("mp4")) category = "Videos";
-   else if (ext.equals("pdf") || ext.equals("txt")) category = "Documents";
-        File newFolder = new File(folderPath + "\\" + category);
-                if (!newFolder.exists()) {
-                    newFolder.mkdir();
-                }
-File newFile = new File(newFolder, f.getName());
- boolean moved = f.renameTo(newFile);
-if (moved) {
-                    System.out.println("Moved " + name + " to " + category);
-       } else {
-                    System.out.println("Could not move " + name);
+if (files == null) 
+    return;
+        HashSet<String> seen = new HashSet<>();
+ System.out.println("\nChecking duplicates...");
+        for (File f : files) {
+            if (f.isFile()) {
+                String name = f.getName();
+                if (seen.contains(name)) {
+                    System.out.println("Duplicate found: " + name);
+                } else {
+                    seen.add(name);
                 }
             }
         }
     }
 }
+
